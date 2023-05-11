7f475b0a0a60408a8ad3e477f83756d8d4de7bba
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-11 10:22:53 +0200

    Initial commit Starter project

---
+++ app/src/main/java/com/example/composenavdemo/HomeScreen.kt
@@ -0,0 +1,55 @@
+package com.example.composenavdemo
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen( modifier: Modifier = Modifier){
+    var text by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(60.dp)
+        ,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ){
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = modifier.fillMaxWidth(),
+            textStyle = TextStyle(
+                color = Color.DarkGray,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+        )
+
+        Button(
+            onClick = {
+
+            },
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Submit",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
