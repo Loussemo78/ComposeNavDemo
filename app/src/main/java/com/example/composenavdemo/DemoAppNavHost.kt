f8b2fc8275991873f8712b382f7ce4e3f26d6724
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-11 10:42:49 +0200

    NavHost composable added

---
+++ app/src/main/java/com/example/composenavdemo/DemoAppNavHost.kt
@@ -0,0 +1,24 @@
+package com.example.composenavdemo
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DemoAppNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController = navController, startDestination = "home_screen"){
+
+        composable(route = "home_screen"){
+            HomeScreen()
+        }
+
+        composable(route = "second_screen"){
+            SecondScreen()
+        }
+    }
+}
--- app/src/main/java/com/example/composenavdemo/HomeScreen.kt
+++ app/src/main/java/com/example/composenavdemo/HomeScreen.kt
@@ -10,11 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen( modifier: Modifier = Modifier){
+fun HomeScreen( navController: NavController, modifier: Modifier = Modifier){
     var text by remember { mutableStateOf("") }
 
     Column(
@@ -53,3 +55,5 @@ fun HomeScreen( modifier: Modifier = Modifier){
         }
     }
 }
+
+
